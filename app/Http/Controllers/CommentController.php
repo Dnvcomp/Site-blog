03f18dc9e5d84f42b2ac4bf75f28a704421dc85e
unknown <dnvcomp@hotmail.com>
2019-05-19 09:33:28 +0300

    Save comment in database

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,6 @@ namespace Dnvcomp\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Dnvcomp\Http\Requests;
-use Illuminate\Http\Response;
 use Validator;
 use Auth;
 use Dnvcomp\Comment;
@@ -43,32 +42,25 @@ class CommentController extends DnvcompController
     {
         $data = $request->except('_token','comment_post_ID','comment_parent');
         $data['article_id'] = $request->input('comment_post_ID');
-        $data['article_id'] = $request->input('comment_parent');
-
+        $data['parent_id'] = $request->input('comment_parent');
         $validator = Validator::make($data,[
             'article_id' => 'integer|required',
             'parent_id' => 'integer|required',
             'text' => 'string|required'
         ]);
-
-        $validator->sometimes(['name','email'],'required|max:255',function ($input) {
+        $validator->sometimes(['name','email'],'required|max:255',function($input) {
             return !Auth::check();
         });
-
-        if ($validator->fails()) {
+        if($validator->fails()) {
             return \Response::json(['error'=>$validator->errors()->all()]);
         }
-
         $user = Auth::user();
         $comment = new Comment($data);
-
         if($user) {
-          $comment->user_id = $user->id;
+            $comment->user_id = $user->id;
         }
-
         $post = Article::find($data['article_id']);
         $post->comments()->save($comment);
-
 
         echo json_encode(['hello'=>'world']);
         exit();
